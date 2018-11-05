d22f8798ab364ca0dac4a801655d8e75dede588d
hzfvictory <777.@qq.com>
2018-11-05 18:58:48 +0800

    搭建服务

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
-import { connect } from 'dva';
-import { Link, routerRedux } from 'dva/router';
-import { NavBar } from 'antd-mobile';
-import PropTypes from 'prop-types';
-import { IntlProvider } from 'react-intl';
-import { Button } from 'antd-mobile';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { WingBlank, Carousel } from 'antd-mobile';
+//组件
+import Banner from '../../components/Banner';
+import MHeader from './components/MHeader';
+import Menu from './components/Menu';
+//上下文
+import NameContext from '../../components/Context';
+import styles from './index.less';
 
-const mapStateToProps = ({ count: { count }}) =>({ count });
+const mapStateToProps = (state) => (state);
+
 @connect(mapStateToProps)
-class index extends Component {
-  state = {};
-  static defaultProps = {};
-  static propTypes = {};
 
-  onClickAddBtn = () => {
-    this.props.dispatch({
-      type: 'home/add',
-    }).then(response=>console.log(response))
+class index extends Component {
+  state = {
+    banner: [],
   };
 
   render() {
-    const { onClickAddBtn, props: { count}} = this;
+    const { props: { home: { banner } }, openDrawer } = this;
     return (
-      <IntlProvider locale="en">
-        <div>
-          <span>{count}</span>
-          <Button
-            onClick={onClickAddBtn}
-          >
-            +
-          </Button>
+      <Fragment>
+        <MHeader onOpen={openDrawer}/>
+        <div className={styles.bg}>
+          <NameContext.Provider value={banner}>
+          </NameContext.Provider>
+          <Banner banner={banner} imgHeight={176}/>
         </div>
-      </IntlProvider>
+        <Menu/>
+      </Fragment>
     );
   }
-}
 
+  openDrawer = state => {
+    this.setState({
+      isDrawer: state,
+    });
+  };
+}
 
 
 export default index;
